58a53a1b8bd1d9dce7a7695c7a010ce1e314c07e
Nataliya Ioffe <44322221+nataliyaioffe@users.noreply.github.com>
2025-08-21 15:22:14 -0400

    studio excellence

---
+++ schemaTypes/artistType.ts
@@ -0,0 +1,23 @@
+import { UsersIcon } from '@sanity/icons'
+import {defineField, defineType} from 'sanity'
+
+export const artistType = defineType({
+  name: 'artist',
+  title: 'Artist',
+  type: 'document',
+  icon: UsersIcon,
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'description',
+      type: 'text',
+    }),
+    defineField({
+      name: 'photo',
+      type: 'image',
+    }),
+  ],
+})
